5b6bf3f52179477bc34906fa1a2f5d23aefb6132
Ben Weintraub <ben@newrelic.com>
2014-07-08 10:28:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnicornTest::Application.config.secret_token = 'baa4b9ca2f6faf076c83fa22711f2b1db6982c701d3b52d7df703c0bda77e687c3c40ee428df595ce08839d009123d7bd0acc46e383f012af2c0861edf6b528c'
